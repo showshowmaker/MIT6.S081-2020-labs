dab7bda9f96712adf5df3d2ff8b0850cce27ba4c
showshowmaker <1642512766@qq.com>
2025-01-11 14:47:48 +0800

    lab9 done

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -316,6 +316,30 @@ sys_open(void)
     }
   }
 
+  int depth = 0;
+  while(ip->type==T_SYMLINK && !(omode & O_NOFOLLOW)){
+    
+    char target[MAXPATH];
+    memset(target, 0, MAXPATH);
+    if(readi(ip, 0, (uint64)target, 0, MAXPATH) < 0){
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+    iunlockput(ip);
+    if((ip = namei(target)) == 0){
+      end_op();
+      return -1;
+    }
+    ilock(ip);
+    depth++;
+    if(depth > 10){
+      iunlockput(ip);
+      end_op();
+      return -1;
+    }
+  }
+
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
     iunlockput(ip);
     end_op();
@@ -482,5 +506,32 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64 sys_symlink(void){
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+  memset(target, 0, MAXPATH);
+  memset(path, 0, MAXPATH);
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  if((ip=namei(path)) != 0){
+    end_op();
+    return -1;
+  }
+  if((ip = create(path, T_SYMLINK, 0, 0)) == 0){
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, (uint64)target, 0, MAXPATH) < 0){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
   return 0;
 }
